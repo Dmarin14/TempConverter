767dce26a2cae230d3c4ff58bd5585579d8bdfbd
David Marin <marindavid019@gmail.com>
2022-06-23 12:25:11 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temp1 = TempConvert.FahrenheitToCelsius(98);
+            double temp2 = TempConvert.CelsiusToFahrenheit(98);
+
+
+            Console.WriteLine($"Fahrenheit to Celsius: {temp1}\nCelsius To Fahrenheit: {temp2}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TempConvert.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempConverter
+{
+    internal static class TempConvert
+    {
+
+
+        public static double FahrenheitToCelsius(double d)
+        {
+            return (d - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double d)
+        {
+            return d * 5 / 9 + 32;
+        }
+    }
+}
